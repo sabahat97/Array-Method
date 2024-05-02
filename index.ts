ec106a332e4fb1a2c83c19e3afa0841e17cf0089
syeda <sbtshakeel61@gmail.com>
2024-05-02 23:23:47 +0500

    first commit

---
+++ index.ts
@@ -0,0 +1,9 @@
+let clothes : string []= ["shirt", "T-shirt", "paints", "shalwar kaleez", "frock"]
+console.log(clothes[1]) // T-shirt
+let a = clothes[3]  //shalwar kaleem
+console.log (a)
+
+//index number of array 0, 1, 2, 3, 4, 5
+console.log (clothes.length)  // output 5
+
+console.log(clothes) //output [ 'shirt', 'T-shirt', 'paints', 'shalwar kaleez', 'frock' ]
---
+++ main.ts
@@ -0,0 +1,10 @@
+let firendNames : string []=["zara", "saba","fara","sara",]
+console.log (firendNames)  // output [ 'zara', 'saba', 'fara', 'sara' ]
+
+//let remove last elements from array by using method pop()
+firendNames.pop()
+console.log (firendNames);  //output [ 'zara', 'saba', 'fara' ]  removed
+
+//let add new array in the last element
+firendNames.push("sabahat");
+console.log (firendNames) //output [ 'zara', 'saba', 'fara', 'sabahat' ] added
